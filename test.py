69ade6841a4628c2dcf297d93bdfd47d48f9cb1d
meline <ab-mel@yandex.ru>
2020-02-06 15:23:54 +0400

    fibanachy

--- test.py
+++ test.py
@@ -3,3 +3,15 @@ print("Another test")
 print("try again")
 a, b = 1, 2
 print(f'{a} + {b} is equal to {a+b}')
+
+
+def fib(num):
+    a, b = 0, 1
+    for i in range(num):
+        print(a)
+        a, b = b, a+b
+
+
+if __name__ == "__main__":
+    fib_num = fib(10)
+
