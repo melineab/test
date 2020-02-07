05d216f2ad568f424047b62ebdec04a90f95c3de
meline <ab-mel@yandex.ru>
2020-02-07 11:14:14 +0400

    hhh

--- test.py
+++ test.py
@@ -1,15 +1,13 @@
 print("This is github test")
 print("Another test")
 print("try again")
-a, b = 1, 2
-print(f'{a} + {b} is equal to {a+b}')
 
 
 def fib(num):
     a, b = 0, 1
     for i in range(num):
         print(a)
-        a, b = b, a+b
+        a, b = b, a + b
 
 
 if __name__ == "__main__":
